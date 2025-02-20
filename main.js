b3f4b82e6bb18916346e6ccc73132ee08f51b321
Kaveesh Ranaweera <rakranaweera051@gmail.com>
2025-02-21 00:31:48 +0530

    Link update

--- main.js
+++ main.js
@@ -13,6 +13,16 @@ function openTab(tabName) {
     document.getElementById(tabName).classList.add('active');
     document.querySelector(`[onclick="openTab('${tabName}')"]`).classList.add('active');
 }
+function openAliExpress() {
+    var appLink = "aliExpress://s.click.aliexpress.com/e/_oERwPu2";
+    var webLink = "https://s.click.aliexpress.com/e/_oERwPu2";
+
+    window.location.href = appLink;
+    
+    setTimeout(() => {
+        window.location.href = webLink;
+    }, 1500); // If the app doesn't open, redirect to the browser link
+}
 
 // Open AliExpress tab by default when page loads
 document.addEventListener("DOMContentLoaded", function() {
